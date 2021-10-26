68a899c9a0c26e8905381f021b75c1e8bb29badd
unknown <kelvin-joel>
2021-10-25 21:42:58 -0500

    web de halloween

--- Js/Index.js
+++ Js/Index.js
@@ -8,4 +8,5 @@ btn_menu.addEventListener('click',()=>{
 
 menu.addEventListener('click',()=>{
     menu.classList.toggle('ChangeClass')
+ btn_menu.classList.contains('fa-bars') ? btn_menu.classList.replace('fa-bars','fa-times') : btn_menu.classList.replace('fa-times','fa-bars')
 })
